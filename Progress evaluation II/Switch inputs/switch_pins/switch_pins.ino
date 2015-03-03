0dfcb066dff975d4fe271e32c4467b5e5f51e42f
saiimaali <saiima.ali@mail.utoronto.ca>
2015-03-03 11:28:33 -0500

    im committed

--- Progress evaluation II/Switch inputs/switch_pins/switch_pins.ino
+++ Progress evaluation II/Switch inputs/switch_pins/switch_pins.ino
@@ -1,8 +1,8 @@
 const int switchPins[4] = {13,12,8,7};
-const int LEDPin = 4;
-int obstacle_1[][2] = {{0,0}, {0,0}, {0,0}};
-int obstacle_2[][2] = {{0,0}, {0,0}, {0,0}};
+const int LEDPin = A2;
+int obstacle[][2] = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}};
 int key_readings[3];
+int reading_index = 0;
 int counter = 0;
 //int obstacle[2] = { obstacle_1, obstacle_2 };
 
@@ -13,6 +13,7 @@ void setup (){
   pinMode( switchPins[3], INPUT );
   
   pinMode( LEDPin, OUTPUT );
+  digitalWrite( LEDPin, LOW );
   Serial.begin( 9600 );
 }  
   
@@ -20,9 +21,35 @@ void loop (){
   int key_pressed = readKeys();
   if( key_pressed ){
     digitalWrite( LEDPin, HIGH );
+    Serial.println( "LED ON DRUGES" );
+    delay( 1000 );
+    digitalWrite( LEDPin, LOW );
+    Serial.println( "LED IS OK NOW" );
+    
+    counter ++;
+    Serial.print( "Counter: " );
+    Serial.println( counter );
   }
-  delay( 1000 );
-  digitalWrite( LEDPin, LOW );
+  if( counter == 3 ) {
+    counter = 0;
+    obstacle[reading_index][0] = key_readings[0] + key_readings[1];
+    Serial.print( "X: " );
+    Serial.println( obstacle[reading_index][0] );
+    obstacle[reading_index][1] = key_readings[2];
+    
+    Serial.print( "Y: " );
+    Serial.println( obstacle[reading_index][1] );
+    reading_index ++;
+    Serial.print( "Reading index" );
+    Serial.println( reading_index );
+    if( reading_index == 6 ){
+      while( 1 ){
+        Serial.print( "Readind index is 6" );
+        delay(10);
+      }
+    }
+  }
+  
   
 }
 
