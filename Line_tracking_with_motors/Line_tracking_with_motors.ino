4757f5a56c7eb663f4b22870451d9a380f71de90
Saima Cat <saiima.ali@mail.utoronto.ca>
2015-03-21 11:13:20 -0400

    Latest working version

--- Line_tracking_with_motors/Line_tracking_with_motors.ino
+++ Line_tracking_with_motors/Line_tracking_with_motors.ino
@@ -16,18 +16,23 @@
 #define BOARDHEIGHT 9
 #define BOARDWIDTH 8
 
-#define THRESHOLD 500
+#define THRESHOLD 200
 #define SERVO_ANGLE 20
 
-#define PWM_INCREMENT 150
-#define LINE_TIME 300
+#define PWM_INCREMENT 70
+#define LINE_TIME 200
 
 // int motor_1_state[3];
 // int motor_2_state[3];
 
-const int motorPin_1_array[3] = { 7, 6, 3};
-const int motorPin_2_array[3] = {10, 11, 9};
-const int line_sensor[2] = {A3, A2};
+//right motor
+//digital, digital, pwm
+const int motorPin_1_array[3] = { 7, 6, 3 };
+//left motor
+//digital, digital, pwm
+const int motorPin_2_array[3] = { 10, 11, 9 };
+//right, left
+const int line_sensor[3] = { A2, A3, A4 }; //the third is the center one
 
 //left A2, 10 11 9
 
@@ -46,31 +51,16 @@ void setup () {
   pinMode( motorPin_2_array[1], OUTPUT );
   //pinMode( motorPin_2_array[2], OUTPUT );
   
-  pinMode( line_sensor[0], INPUT );
-  pinMode( line_sensor[1], INPUT );
+  //pinMode( line_sensor[0], INPUT );
+  //pinMode( line_sensor[1], INPUT );
 
   Serial.begin( 9600 );
   
-
 }
 
 void loop () {
-  lineFollow(90);
-  /**Serial.print( "x: " );
-  Serial.println( myPos[0] );
-  Serial.print( "y : " );
-  Serial.println( myPos[1] );**/
-  
-  int line_1_val = analogRead( line_sensor[0] );
-  int line_2_val = analogRead( line_sensor[1] );
-  
-  Serial.print( "IR 1: " );
-  Serial.println( line_1_val );
-  Serial.print( "IR 2: " );
-  Serial.println( line_2_val );
-  
-  //delay( 300 );
-  
+  lineFollow(80);
+
 }
 
 // Coordinates
@@ -123,7 +113,91 @@ int AmIThereYet( int target ) {
   return ( myPos[index] == target );
 }
 
-void lineFollow(byte PWM_val) {
+void lineFollow( byte PWM_val ) {
+  
+  int line_1_val = analogRead( line_sensor[0] );
+  int line_2_val = analogRead( line_sensor[1] );
+  int line_3_val = analogRead( line_sensor[2] );
+  
+  driveForward( motorPin_1_array, PWM_val + PWM_INCREMENT );
+  driveForward( motorPin_2_array, PWM_val );
+  
+  while ( line_2_val < THRESHOLD ) {
+    line_2_val = analogRead( line_sensor[1] );
+    continue;
+    
+  } 
+  //start to the right, wait till left sensor is hit
+  // left sensor is hit
+  
+  Serial.println( "Left on black" );
+  
+  driveStop( motorPin_1_array );
+  driveStop( motorPin_2_array );
+  
+  delay( 1000 );
+  
+  driveStop( motorPin_1_array );
+  driveForward( motorPin_2_array, PWM_val );
+  
+  //pivot
+  line_3_val = analogRead( line_sensor[2] );
+  Serial.println( "Reading 3" );
+  
+  while( line_3_val < THRESHOLD ) {
+    line_3_val = analogRead( line_sensor[2] );
+  }
+  
+  Serial.println( line_3_val );
+  
+  driveStop( motorPin_1_array );
+  driveStop( motorPin_2_array );
+  
+  delay( 1000 );
+  
+  line_2_val = analogRead( line_sensor[1] );
+  
+  Serial.println( line_2_val );
+  
+  driveForward( motorPin_2_array, PWM_val + PWM_INCREMENT);
+  driveForward( motorPin_1_array,  PWM_val );
+  
+  while( line_2_val < THRESHOLD ) {
+    line_2_val = analogRead( line_sensor[1] );
+    Serial.println( "NOT" );
+    delay( 30 );  
+}
+  // waiting to see if it gets back on the line
+  //now, wait for it to overshoot
+
+  /*while( line_2_val >= THRESHOLD ) {
+    line_2_val = analogRead( line_sensor[1] );
+    Serial.println( "LINE" );
+    delay( 30 );
+  }*/
+  
+  driveStop( motorPin_1_array );
+  driveStop( motorPin_2_array );
+  delay( 1000 );
+  
+  //pivot
+  
+  driveForward( motorPin_1_array, PWM_val );
+  driveStop( motorPin_2_array );
+  
+  line_3_val = analogRead( line_sensor[2] );
+  while( line_3_val < THRESHOLD ){
+    delay( 10 );
+    line_3_val = analogRead( line_sensor[2] );
+  }
+  
+  driveStop( motorPin_1_array );
+  
+  delay( 10000 );
+  
+}
+
+/**void lineFollow(byte PWM_val) {
   int time_to_black_1 = 0;
   int time_to_black_2 = 0;
   
@@ -147,7 +221,6 @@ void lineFollow(byte PWM_val) {
     //  delay( 20 );
     //}
     
-    
   } else if ( line_1_val < THRESHOLD && line_2_val < THRESHOLD ) {
     
     Serial.println( "On white" );
@@ -159,55 +232,23 @@ void lineFollow(byte PWM_val) {
     
     Serial.println( "Line sensor 2 is off " );
     time_to_black_2 = millis();
-    if( abs( time_to_black_2 - time_to_black_1 ) > LINE_TIME ) {
     
-      driveForward( motorPin_1_array, PWM_val + PWM_INCREMENT );
+    //if( abs( time_to_black_2 - time_to_black_1 ) > LINE_TIME ) {
+      driveForward( motorPin_1_array, PWM_val + PWM);
       driveForward( motorPin_2_array, PWM_val );
-    }
-  }
-    /*
-    delay( 300 );
-    
-    driveForward( motorPin_1_array, PWM_val );
-    
-    delay( 400 );
-    */
-    //driveStop( motorPin_2_array );
-    /*
-    delay( 500 );
-    
-    driveForward( motorPin_2_array, PWM_val );
-    
-    delay( 500 );
-    */
+
   } else if ( line_1_val > THRESHOLD && line_2_val < THRESHOLD ) {
-    
     Serial.println( "Line sensor 1 is off " );
     
     time_to_black_1 = millis();
     
-    if( abs( time_to_black_2 - time_to_black_1 ) > LINE_TIME ) {
+    //if( abs( time_to_black_2 - time_to_black_1 ) > LINE_TIME ) {
       driveForward( motorPin_2_array, PWM_val + PWM_INCREMENT );
       driveForward( motorPin_1_array, PWM_val );
+      
     }
-    /*
-    delay( 300 );
-    
-    driveForward( motorPin_2_array, PWM_val );
-    
-    delay( 400 );
-    */
-    /*
-    driveStop( motorPin_1_array);
-    
-    delay( 500 );
-    
-    driveForward( motorPin_1_array, PWM_val );
-    
-    delay( 500 );
-    */
-  }
-}
+      
+}**/
 
 /*
 void getToWall() {
